5392f2a0292cf08d037a8b4094a9065c556cf8cb
Masagus Hariadi Arief <adi.arief77@gmail.com>
2021-04-27 13:52:33 +0700

    grid

--- src/views/sales/TabFood.vue
+++ src/views/sales/TabFood.vue
@@ -1,12 +1,33 @@
 <template>
-  <h1>Food</h1>
+	<div class="food">
+		<h1>Food</h1>
+		<div class="grid cell-6 cell-850-12 cell-450-6 cell-380-3">
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+		</div>
+	</div>
 </template>
 
 <script lang='ts'>
 export default {
-  name: "Food",
-};
+	name: 'Food',
+}
 </script>
 
-<style>
+<style lang="scss" scoped>
+.food {
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+}
 </style>
